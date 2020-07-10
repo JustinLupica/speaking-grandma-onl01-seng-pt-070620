874ec0b897954e8605abcd339a609ce67e336dfa
Justin Lupica <justinlupica@gmail.com>
2020-07-10 01:12:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma
   speak = gets.chomp
   
-if speak = "I LOVE YOU GRANDMA!"
+if speak == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-  elsif speak = speak.upcase
+  elsif speak == speak.upcase
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
